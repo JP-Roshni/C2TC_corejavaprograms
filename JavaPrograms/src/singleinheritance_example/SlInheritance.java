34a46b65f90be4499fe96a6ca480df326a96ee08
JP-Roshni <roshnikalpu@gmail.com>
2025-08-19 14:10:46 +0530

    Single Inheritance uploaded

---
+++ JavaPrograms/src/singleinheritance_example/SlInheritance.java
@@ -0,0 +1,12 @@
+package singleinheritance_example;
+
+public class SlInheritance {
+
+	public static void main(String[] args) {
+		Trainer t=new Trainer(112,"Roshni","Villupuram",56,"lead","Pondicherry");
+		System.out.println(t);
+		Person p=new Person(223,"Roshni", "Villupuram");
+		System.out.println(p);
+	}
+
+}
