d350ec1ed18f8f241ce9a8e6a2887e090effea2b
JP-Roshni <roshnikalpu@gmail.com>
2025-08-22 10:18:07 +0530

    Interface Example

---
+++ JavaPrograms/src/shoppingmall/Cloths.java
@@ -0,0 +1,7 @@
+package shoppingmall;
+
+public class Cloths implements Shopping {
+	public void purchase() {
+		System.out.println("im going to purchase cloths");
+	}
+}
---
+++ JavaPrograms/src/shoppingmall/ShoppingMallDemo.java
@@ -0,0 +1,11 @@
+package shoppingmall;
+
+public class ShoppingMallDemo {
+
+	public static void main(String[] args) {
+		Shopping s;
+		s=new Cloths();
+		s.purchase();
+	}
+
+}
